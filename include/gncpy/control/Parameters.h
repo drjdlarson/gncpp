43b6ebeb22e3ab185ef090a087d3e035fe37fcae
vaweirens <vaweirens@crimson.ua.edu>
2023-07-11 22:56:18 -0400

    initial control commit

---
+++ include/gncpy/control/Parameters.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <cereal/access.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/polymorphic.hpp>
+
+#include "gncpy/SerializeMacros.h"
+
+namespace lager::gncpy::control {
+
+    class ControlParams {
+        friend class cereal::access;
+
+        GNCPY_SERIALIZE_CLASS(ControlParams)
+
+        public:
+            virtual ~ControlParams() = default;
+        
+        private:
+            template <class Archive>
+            void serialize([[maybe_unused]] Archive& ar) {
+                /* nothing to serialize*/
+            }
+    };
+
+}  //  namespace lager::gncpy::control
+
+CEREAL_REGISTER_TYPE(lager::gncpy::control::ControlParams)
