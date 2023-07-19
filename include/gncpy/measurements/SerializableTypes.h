ed3f44fa1bcc5f6d3e826cca32533a219977e29c
vaweirens <vaweirens@crimson.ua.edu>
2023-07-19 23:49:23 +0000

    removed serializable types

--- include/gncpy/measurements/SerializableTypes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "gncpy/measurements/ILinearMeasModel.h"
-#include "gncpy/measurements/IMeasModel.h"
-#include "gncpy/measurements/INonLinearMeasModel.h"
-#include "gncpy/measurements/Parameters.h"
-#include "gncpy/measurements/RangeAndBearing.h"
-#include "gncpy/measurements/StateObservation.h"
